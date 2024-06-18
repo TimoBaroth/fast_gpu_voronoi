7a5dd4c0837466bc89bf50fbaf37363c0e04ae03
TimoBaroth <129017692+TimoBaroth@users.noreply.github.com>
2024-06-18 23:56:58 +1000

    a

--- gpu_voronoi_example.py
+++ gpu_voronoi_example.py
@@ -1,12 +1,13 @@
 from fast_gpu_voronoi       import Instance
-from fast_gpu_voronoi.jfa   import JFA_star
+from fast_gpu_voronoi.jfa   import JFA
 from fast_gpu_voronoi.debug import save
 
 
-I = Instance(alg=JFA_star, x=50, y=50, \
+I = Instance(alg=JFA, x=1200, y=1200, \
         pts=[[ 7,14], [33,34], [27,10],
              [35,10], [23,42], [34,39]])
 I.run()
+
 
 print(I.M.shape)                 # (50, 50, 1)
 save(I.M, I.x, I.y, force=True)  # __1_debug.png0
